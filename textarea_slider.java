dbc354bee35461bcc24abea843232f77b5823597
rinki <rinkimittal20062GAMIL.COM>
2026-06-02 13:33:49 +0530

    Initial commit

--- textarea_slider.java
+++ textarea_slider.java
@@ -3,19 +3,31 @@ package javafx;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Slider;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.scene.control.*;
+import javafx.scene.text.Font;
 
-import java.awt.*;
 
 public class textarea_slider extends Application {
     TextArea ta;
     Slider s;
     public void start(Stage stage) throws Exception{
+        ta = new TextArea();
+        ta.setPrefColumnCount(100);
+        ta.setPrefRowCount(20);
 
-        Scene sc = new Scene();
+        s = new Slider(10,50,10);
+        VBox v = new VBox();
+        v.getChildren().addAll(ta, s);
 
+        s.valueProperty().addListener(e-> ta.setFont(Font.font(s.getValue())));
+        Scene sc = new Scene(v, 400,400);
+        stage.setScene(sc);
+        stage.show();;
     }
     public static void main(String[] args) {
+
         launch(args);
     }
 }
